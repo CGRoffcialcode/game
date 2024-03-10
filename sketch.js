31aa435bb3f4f028ebbf064f0855026af5d128e5
CGRoffcialcode <148871225+CGRoffcialcode@users.noreply.github.com>
2024-03-10 13:28:54 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,28 +26,28 @@ function draw(){
       writePosition(-1,0);
       data = {
         x:-1,
-        y:0
+        y:+0
       }
     }
     else if(keyDown(RIGHT_ARROW)){
       writePosition(1,0);
       data = {
-        x:1,
-        y:0
+        x:+1,
+        y:+0
       }
     }
     else if(keyDown(UP_ARROW)){
       writePosition(0,-1);
       data = {
-        x:0,
+        x:+0,
         y:-1
       }
     }
     else if(keyDown(DOWN_ARROW)){
       writePosition(0,+1);
       data = {
-        x:0,
-        y:1
+        x:+0,
+        y:+1
       }
     }
     drawSprites();
@@ -58,7 +58,7 @@ function writePosition(x,y){
   database.ref("ball/position").set({
     'x':position.x+x,
     'y':position.y+y,
-    data:position.data+data,
+    data:position.data+data
   })
   
 }
@@ -66,7 +66,9 @@ function writePosition(x,y){
 function readPosition(data){
   position = data.val();
   console.log(position.x);
+  console.log(position.data);
   ball.x = position.x;
+  ball.y = position.y;
   data = position.data;
 }
 
